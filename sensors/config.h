0585dd51d3723e7e899fcb81baf83e5d934ad60b
Mathieu LESNIAK <mathieu@lesniak.fr>
2014-11-18 22:10:42 +0100

    [New] Config file

---
+++ sensors/config.h
@@ -0,0 +1,11 @@
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+const int pinResetConfig  = 2;
+const int pinTemperature  = 7; // Broche utilisée pour le bus 1-Wire
+const int pinBattery      = A0;  // select the input pin for the battery sense point
+const int pinRfCse        = 9;
+const int pinRfCsn        = 10;
+
+
+#endif
