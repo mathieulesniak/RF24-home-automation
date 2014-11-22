f874e7666b7eb169d835fb63323e628bae9afa89
Mathieu LESNIAK <mathieu@lesniak.fr>
2014-11-22 17:03:16 +0100

    [Upg] Remove payload struct + func defintion

--- sensors/sensorsFunctions.h
+++ sensors/sensorsFunctions.h
@@ -3,15 +3,7 @@
 
 #define DS18B20 0x28     // Adresse 1-Wire du DS18B20
 
-struct payload_t      // 32 bytes max
-{ 
-  uint32_t sentPacketCounter;
-  uint32_t failedPacketCounter;
-  byte batteryPercentage;      // 1 byte
-  uint16_t sensor[3];
-};
-
-void assignPayloadSensorValue(nodeEepromConfig* nodeConfig, payload_t* payload);
+void assignPayloadSensorValue(nodeEepromConfig* nodeConfig, sensorMessagePayload* payload);
 
 bool sensorBatteryLevel(byte* Percentage);
 bool sensorTemperature(uint16_t* temp);
